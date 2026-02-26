26684dfa3ed18ae7b754c18c5874a17e925eb52a
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-02-25 20:43:26 -0500

    Se crea la clase DataContext para la base de datos

---
+++ backend/Context/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using backend.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace backend.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Menu> Menu { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "Admin", Email = "Admin@admin.com", Password = "123457", Image = "", UserType = UserType.Admin }
+            );
+            modelBuilder.Entity<Menu>().HasData(
+                new Menu { Id = 1, Text = "Home", Page = "/", Icon = "" }
+            );
+        }
+
+    }
+}
