63494b0393043e5304cf0ec11baf26cdf131480a
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-02-25 20:43:45 -0500

    Se crea el controlador del Menu

---
+++ backend/Controllers/MenuController.cs
@@ -0,0 +1,41 @@
+﻿using backend.Context;
+using backend.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public MenuController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public Response GetMenu()
+        {
+            Response response = new();
+            var code = 200;
+            try
+            {
+                response.Data = _context.Menu.ToList();
+            }
+            catch
+            {
+                code = 500;
+                response.Message = "Sorry something went wrong, try again later";
+                response.Data = null;
+            }
+            response.StatusCode = code;
+            return response;
+        }
+
+
+
+    }
+}
