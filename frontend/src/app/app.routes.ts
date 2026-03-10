c95096b62c6a54309e179c9483369e6cff57f6c1
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-03-09 21:27:53 -0500

    Creación de rutas bases del app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { User } from './models/user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login },
+  { path: 'user', component: User },
+  { path: '**', redirectTo: '' }
+];
